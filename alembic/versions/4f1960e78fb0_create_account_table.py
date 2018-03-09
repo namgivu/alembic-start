efc3bd6bdd4794c7964c166a39b3c39720f62567
namgivu <namgivu@gmail.com>
2018-03-09 23:55:00 +0700

    alembic revision -m 'create account table'

---
+++ alembic/versions/4f1960e78fb0_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 4f1960e78fb0
+Revises: 
+Create Date: 2018-03-09 23:51:43.787055
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4f1960e78fb0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
